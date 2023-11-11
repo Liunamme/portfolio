1ff82fd9bb5441add11be9ec944ed4d192e97353
Zadorozhnyy Emmanuel <120856468+Liunamme@users.noreply.github.com>
2023-11-11 14:43:59 +0300

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
       <Home />
       <About/>
       <HardSkills/>
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,31 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from '@svgr/rollup';
-import path from 'path'; // Импорт модуля path
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), svgr()],
-  base: 'https://liunamme.github.io/portfolio/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'src/main.jsx'), // Используйте path.resolve для создания абсолютного пути
-        // ... другие точки входа
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@icons': '/public/assets/media/icons/',
-      '@img': '/public/assets/media/img',
-    },
-  },
-});
+  plugins: [react()],
+  base: 'https://Liunamme.github.io/portfolio/',
+})
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import svgr from '@svgr/rollup';
+// import path from 'path'; // Импорт модуля path
+
+// export default defineConfig({
+//   plugins: [react(), svgr()],
+//   base: 'https://liunamme.github.io/portfolio/',
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: path.resolve(__dirname, 'src/main.jsx'), // Используйте path.resolve для создания абсолютного пути
+//         // ... другие точки входа
+//       },
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       '@icons': '/public/assets/media/icons/',
+//       '@img': '/public/assets/media/img',
+//     },
+//   },
+// });
