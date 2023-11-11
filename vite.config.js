3c808e6b3aec18bd34de2d93842b25fa327ad3cb
Zadorozhnyy Emmanuel <120856468+Liunamme@users.noreply.github.com>
2023-11-11 14:48:33 +0300

    update files

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from '@svgr/rollup';
+import path from 'path'; // Импорт модуля path
 
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://Liunamme.github.io/portfolio/',
-})
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import svgr from '@svgr/rollup';
-// import path from 'path'; // Импорт модуля path
-
-// export default defineConfig({
-//   plugins: [react(), svgr()],
-//   base: 'https://liunamme.github.io/portfolio/',
-//   build: {
-//     rollupOptions: {
-//       input: {
-//         main: path.resolve(__dirname, 'src/main.jsx'), // Используйте path.resolve для создания абсолютного пути
-//         // ... другие точки входа
-//       },
-//     },
-//   },
-//   resolve: {
-//     alias: {
-//       '@icons': '/public/assets/media/icons/',
-//       '@img': '/public/assets/media/img',
-//     },
-//   },
-// });
+  plugins: [react(), svgr()],
+  base: 'https://liunamme.github.io/portfolio/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/main.jsx'), // Используйте path.resolve для создания абсолютного пути
+        // ... другие точки входа
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@icons': '/public/assets/media/icons/',
+      '@img': '/public/assets/media/img',
+    },
+  },
+});
